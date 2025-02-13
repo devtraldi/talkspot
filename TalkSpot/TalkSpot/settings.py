677d61b5401bbcc5d7f40b8407e1348250c88dbc
usroliveira <us.roliveira@formacao.ips.pt>
2025-02-13 12:35:37 +0000

    style curtir e reorganizar diretorios .css

--- TalkSpot/TalkSpot/settings.py
+++ TalkSpot/TalkSpot/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Diretório global de arquivos estáticos
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
